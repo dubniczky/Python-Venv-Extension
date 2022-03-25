fe6debf0a3d4d979f5a2dd92b37a1f57a69820a4
Richard Nagy <nagy.richard.antal@gmail.com>
2022-03-25 20:46:04 +0100

    Updated variables to env names

--- venv.sh
+++ venv.sh
@@ -2,13 +2,9 @@
 
 venv.load()
 {
-    VENV_NAME=".venv"
-    DEPS_NAME="requirements.txt"
-    LOCK_NAME="pip.lock"
-
     # Create venv
     echo "[1/5] Creating virtual environment... ($VENV_NAME)"
-    python -m venv $VENV_NAME
+    python -m venv "$VENV_NAME"
 
     # Activate environment
     echo "[2/5] Activating environment..."
@@ -16,13 +12,13 @@ venv.load()
 
     # Install packages
     echo "[3/5] Installing packages..."
-    echo "${pwd}/$DEPS_NAME"
-    pip install -r $DEPS_NAME
+    echo "${pwd}/$VENV_DEPS_NAME"
+    pip install -r $VENV_DEPS_NAME
 
     # Save used package versions
     echo "[4/5] Creating lock file..."
-    echo "${pwd}/$LOCK_NAME"
-    pip freeze > $LOCK_NAME
+    echo "${pwd}/$VENV_LOCK_NAME"
+    pip freeze > $VENV_LOCK_NAME
 
     echo "[5/5] Virtual environment ready."
     echo "${pwd}/$VENV_NAME"
